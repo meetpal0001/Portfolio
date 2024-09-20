aa173ec309ec685f310e4d40702aec7071ea6150
Meetpal Singh <msingh683@myseneca.ca>
2024-09-19 20:45:17 -0400

    resolved bugs

--- src/Skills.js
+++ src/Skills.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import skills from './data/skills'
 
-const SkillIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
-  </svg>
-);
+
 
 const Skill = ({ skill }) => (
   <div className="skill">
